5fe519cc87a916afa161808e6fea34e1001de9fc
Jordan Hall <jordan@hall05.co.uk>
2018-05-07 19:30:41 +0100

    Improve tests

--- tests/Unit/RuleTest.php
+++ tests/Unit/RuleTest.php
@@ -18,17 +18,22 @@ class RuleTest extends TestCase
         $values = [
             'xxxSexyxxx',
             'Big-s-e-x-y',
-            'D*I*C*K*s',
+            'DICK',
             'shitcrapper500',
-            'c-u-n-t-asaurus'
+            'c-u-n-t-asaurus',
+            'm0therfuker',
+            'firstFaggot',
+            'Sh1tface',
+            'Twatter9000',
+            'Ultim8Cun7'
         ];
 
         foreach ($values as $value) {
-            $this->assertFalse($this->getValidator($value)->passes());
+            $this->assertFalse($this->getValidator($value)->passes(), 'Failed asserting that \''.$value.'\' is offensive.');
         }
     }
 
-    public function testUnoffensiveValues()
+    public function testNotOffensiveValues()
     {
         $values = [
             'test',
@@ -43,7 +48,7 @@ class RuleTest extends TestCase
         ];
 
         foreach ($values as $value) {
-            $this->assertTrue($this->getValidator($value)->passes());
+            $this->assertTrue($this->getValidator($value)->passes(), 'Failed asserting that \''.$value.'\' is not offensive.');
         }
     }
 }
